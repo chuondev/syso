b4262024f14ac4095cc2ea0d36529639a388af1a
chuonye <chuonye@foxmail.com>
2021-01-31 11:27:05 +0800

    修复未设置 JAVA_HOME 但存在java环境（/usr/bin/java）获取不到的问题

--- src/support/file/sysod.sh
+++ src/support/file/sysod.sh
@@ -6,11 +6,33 @@ usage() {
 }
 [ $# -gt 0 ] || usage
 
-if [ -z "$JAVA_HOME" ]; then
-  echo "请设置 JAVA_HOME"
-  exit 1
+# 确认 java 环境
+if [ -z "$JAVA_HOME" -a -z "$JRE_HOME" ]; then
+  JAVA_PATH=`which java 2>/dev/null`
+  if [ "x$JAVA_PATH" != "x" ]; then
+    JAVA_PATH=`dirname $JAVA_PATH 2>/dev/null`
+    JRE_HOME=`dirname $JAVA_PATH 2>/dev/null`
+  fi
+  if [ "x$JRE_HOME" = "x" ]; then
+    # XXX: Should we try other locations?
+    if [ -x /usr/bin/java ]; then
+      JRE_HOME=/usr
+    fi
+  fi
+
+  if [ -z "$JAVA_HOME" -a -z "$JRE_HOME" ]; then
+    echo "请设置 JAVA_HOME 或者 JRE_HOME"
+    exit 1
+  fi
 fi
-RUNJAVA="$JAVA_HOME"/bin/java
+
+if [ -z "$JRE_HOME" ]; then
+  JRE_HOME="$JAVA_HOME"
+fi
+
+if [ -z "$_RUNJAVA" ]; then
+  _RUNJAVA="$JRE_HOME"/bin/java
+fi
 
 # 设置程序变量
 PRGDIR=`dirname "$0"`
@@ -19,19 +41,17 @@ SYSO_TMPDIR=$SYSO_BASE/temp
 SPRING_CONFIG=$SYSO_BASE/conf/
 APP_JAR=$SYSO_BASE/lib/syso.jar
 
-JAVA_OPTS="-server -Xms128m -Xmx128m -Xss228k -Xmn64m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -XX:+UseSerialGC"
+JAVA_OPTS="-Xms128m -Xmx128m -Xss228k -Xmn64m -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=128m -XX:+UseSerialGC"
 JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true  -XX:MaxDirectMemorySize=10m -Djava.security.egd=file:/dev/./urandom"
-JAVA_OPTS="$JAVA_OPTS -XX:+AlwaysPreTouch -XX:-TieredCompilation -XX:+UseCompressedOops -XX:+UseCompressedClassPointers"
 JAVA_OPTS="$JAVA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$SYSO_TMPDIR/java_heapdump_pid_%p.log"
-JAVA_OPTS="$JAVA_OPTS -Djava.security.egd=file:/dev/./urandom"
 
 echo Using SYSO_BASE:   $SYSO_BASE
 echo Using SYSO_TMPDIR: $SYSO_TMPDIR
-echo Using JRE_HOME:    $JAVA_HOME
+echo Using JRE_HOME:    $JRE_HOME
 
 pid=
 # 检查是否正在运行
-function running() {
+running() {
   pid=`ps auxww| grep "$APP_JAR" | grep -v grep | awk '{print $2}'`
   if [ -n "$pid" ];then
     return 0
@@ -48,7 +68,7 @@ case "$ACTION" in
       exit 1
     fi
 
-    nohup "$RUNJAVA" $JAVA_OPTS \
+    nohup "$_RUNJAVA" $JAVA_OPTS \
       -Dsyso.base="$SYSO_BASE" \
       -Djava.io.tmpdir="$SYSO_TMPDIR" \
       -Dspring.config.location="$SPRING_CONFIG" \
